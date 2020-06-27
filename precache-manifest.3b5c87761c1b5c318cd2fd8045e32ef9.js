8f381976ef2ddd320aedb9d4ec95077be5776fe6
Daniel Cruz <dcruz.js@gmail.com>
2020-06-27 16:12:03 -0600

    Updates

---
+++ precache-manifest.3b5c87761c1b5c318cd2fd8045e32ef9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872c183ad83c361bd51593c9894a7ee6",
+    "url": "/react-badges-system/index.html"
+  },
+  {
+    "revision": "1639064076320160650a",
+    "url": "/react-badges-system/static/css/2.85f22f3a.chunk.css"
+  },
+  {
+    "revision": "347a22462cdf5baafc14",
+    "url": "/react-badges-system/static/css/main.bcd50d48.chunk.css"
+  },
+  {
+    "revision": "1639064076320160650a",
+    "url": "/react-badges-system/static/js/2.8e8c1981.chunk.js"
+  },
+  {
+    "revision": "707e6f0eb595080bc55d0bb4d5cad864",
+    "url": "/react-badges-system/static/js/2.8e8c1981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "347a22462cdf5baafc14",
+    "url": "/react-badges-system/static/js/main.652d4336.chunk.js"
+  },
+  {
+    "revision": "21e1a0c67bb1466b08b6",
+    "url": "/react-badges-system/static/js/runtime-main.d7a16ec4.js"
+  },
+  {
+    "revision": "71addf454967249472471f5398055e3d",
+    "url": "/react-badges-system/static/media/astronauts.71addf45.svg"
+  },
+  {
+    "revision": "973f5842f0c37eacb2eb491c62fc0032",
+    "url": "/react-badges-system/static/media/badge-header.973f5842.svg"
+  },
+  {
+    "revision": "0026b862f1b05c3e27d471ba85c6ebc5",
+    "url": "/react-badges-system/static/media/logo.0026b862.svg"
+  },
+  {
+    "revision": "f246f5f1fb702016606ee8c65e33c029",
+    "url": "/react-badges-system/static/media/platziconf-logo.f246f5f1.svg"
+  },
+  {
+    "revision": "1d17360a685ac86023b90aa14fd2c62d",
+    "url": "/react-badges-system/static/media/stars.1d17360a.svg"
+  }
+]);
